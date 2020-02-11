806284d09a35a9a61c2ccafa9447468d4f73359e
Student <example@example.com>
2020-02-10 16:10:19 -0800

    Correct margin on h4

--- src/Friends.jsx
+++ src/Friends.jsx
@@ -3,9 +3,13 @@ import Friend from './Friend';
 
 
 function Friends(){
+  const h4Style = {
+    'margin-top': "10px",
+    'margin-bottom': "0px"
+  }
   return (
     <div className="friends">
-    <h4>Lorem Ipsum</h4>
+    <h4 style={h4Style}>Lorem Ipsum</h4>
     <Friend name="Donec eu orci et" color="#f3dbc4"/>
     <Friend name="Donec eu orci et" color="#91bd7e"/>
     <Friend name="Donec eu orci et" color="#4cb3b2"/>
